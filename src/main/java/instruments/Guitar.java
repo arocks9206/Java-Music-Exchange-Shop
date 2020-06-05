43ed92af9f86fb1dfa0c0e491cebec72b5d76519
Alexandria Rocks <alexandria.m.rocks@gmail.com>
2020-06-05 17:28:10 +0100

    adds MusicShop functionality

--- src/main/java/instruments/Guitar.java
+++ src/main/java/instruments/Guitar.java
@@ -1,14 +1,20 @@
 package instruments;
 
 import behaviors.IPlay;
+import behaviors.ISell;
 
-public class Guitar extends Instrument implements IPlay {
+public class Guitar extends Instrument implements IPlay, ISell {
 
     private int noStrings;
+    private double sellPrice;
+    private double buyPrice;
 
-    public Guitar(String material, String color, InstrumentType type, int noStrings) {
+    public Guitar(String material, String color, InstrumentType type,
+                  int noStrings, double sellPrice, double buyPrice) {
         super(material, color, type);
         this.noStrings = noStrings;
+        this.sellPrice = sellPrice;
+        this.buyPrice = buyPrice;
     }
 
     public String play(){
@@ -20,6 +26,7 @@ public class Guitar extends Instrument implements IPlay {
     }
 
 
-
-
+    public double calculateMarkUp() {
+        return this.sellPrice - this.buyPrice;
+    }
 }
